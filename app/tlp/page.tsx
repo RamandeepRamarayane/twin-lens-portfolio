43b46afcf06b6d8063e20a2efbc549b3e4fd2390
Ramandeep Ramarayane <ramandeep.r49@email.com>
2026-06-28 18:09:21 +0530

    tlp page

---
+++ app/tlp/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-50">
+        Twin Lens Production | TLP
+      </h1>
+    </div>
+  );
+}
